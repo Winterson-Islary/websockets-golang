eb56301ab0ebde97825ffa5a721d601bc382472f
Winterson <u19cse1028@cit.ac.in>
2025-01-13 16:23:09 +0530

    add event.go for event specific data

---
+++ internal/server/event.go
@@ -0,0 +1,19 @@
+package server
+
+import "encoding/json"
+
+const (
+	EventSendMessage = "send_message"
+)
+
+type EventHandler func(event Event, c *Client) error
+
+type Event struct {
+	Type    string          `json:"type"`
+	Payload json.RawMessage `json:"payload"` // Not constraining the payload by assigning a type
+}
+
+type SendMessageEvent struct {
+	Message string `json:"message"`
+	From    string `json:"from"`
+}
