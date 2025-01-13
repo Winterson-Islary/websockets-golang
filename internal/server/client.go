882f6b10f1e2650254fe84ca2c6021c8b0485e86
Winterson <u19cse1028@cit.ac.in>
2025-01-13 16:21:34 +0530

    add client read and write message ability

--- internal/server/client.go
+++ internal/server/client.go
@@ -1,13 +1,71 @@
 package server
 
-import "github.com/gorilla/websocket"
+import (
+	"encoding/json"
+	"log"
+
+	"github.com/gorilla/websocket"
+)
 
 type Client struct {
-	Hub  *Hub
-	Conn *websocket.Conn
-	Msg  chan []byte
+	Hub    *Hub
+	Conn   *websocket.Conn
+	Egress chan Event // helps avoid concurrent writes to the websocket connection
+}
+
+func (client *Client) readMessages() {
+	defer func() {
+		client.Hub.removeClient(client)
+	}()
+
+	for {
+		_, payload, err := client.Conn.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Printf("error reading message: %v", err)
+			}
+			break
+		}
+		var request Event
+		if err := json.Unmarshal(payload, &request); err != nil {
+			log.Printf("failed to unmarshal payload: %v", err)
+			break
+		}
+
+		if err := client.Hub.checkEvent(request, client); err != nil {
+			log.Printf("invalid event: %v", err)
+		}
+	}
+}
+
+func (client *Client) writeMessages() {
+	defer func() {
+		client.Hub.removeClient(client)
+	}()
+
+	for {
+		select {
+		case message, ok := <-client.Egress:
+			if !ok {
+				if err := client.Conn.WriteMessage(websocket.CloseMessage, nil); err != nil {
+					log.Println("connection closed: ", err)
+				}
+				return
+			}
+			data, err := json.Marshal(message)
+			if err != nil {
+				log.Printf("failed to marshal message: %v", err)
+				return
+			}
+
+			if err := client.Conn.WriteMessage(websocket.TextMessage, data); err != nil {
+				log.Printf("failed to send message: %v", err)
+			}
+			log.Println("message sent")
+		}
+	}
 }
 
 func NewClient(conn *websocket.Conn, hub *Hub) *Client {
-	return &Client{Conn: conn, Hub: hub}
+	return &Client{Conn: conn, Hub: hub, Egress: make(chan Event)}
 }
