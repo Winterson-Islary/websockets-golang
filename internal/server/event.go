fbd5ff0c8984890706eaedd3f807bdd10b47ffe6
Winterson <u19cse1028@cit.ac.in>
2025-01-15 02:12:32 +0530

    add chatroom events

--- internal/server/event.go
+++ internal/server/event.go
@@ -8,6 +8,7 @@ import (
 const (
 	EventSendMessage = "send_message"
 	EventNewMessage  = "new_message"
+	EventChangeRoom  = "change_room"
 )
 
 type EventHandler func(event Event, c *Client) error
@@ -25,4 +26,8 @@ type SendMessageEvent struct {
 type NewMessageEvent struct {
 	SendMessageEvent
 	Sent time.Time `json:"sent"`
+}
+
+type ChangeRoomEvent struct {
+	Name string `json:"name"`
 }
